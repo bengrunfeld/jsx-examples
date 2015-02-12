eac1f1830167f230ba4b5235dbf2ea3c38a92482
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-02-12 14:57:15 -0800

    init

---
+++ basic_assignment.jsx
@@ -0,0 +1,17 @@
+class _Main {
+    static function main(args : string[]) : void {
+
+        var s : string;
+        var n : number;
+        var b : boolean;
+
+        // You need to initialize the variable before use
+        s = "hi";
+        n = 42;
+        b = false; 
+
+        log s + '\n' + n, b;
+
+    }
+}
+
